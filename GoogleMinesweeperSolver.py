97891a4d598f443ee686618372f4aa033412cf28
Gene Pan <pangene@gmail.com>
2021-01-10 23:25:58 -0800

    Back at it. Made the identifying board stuff work + cells now have coordinates + neighbors.

--- GoogleMinesweeperSolver.py
+++ GoogleMinesweeperSolver.py
@@ -2,13 +2,19 @@
 
 Author: Gene Pan
 Date Began: Aug-02-2020
-Last Edited: Aug-05-2020 8:53 PM
+Last Edited: Jan-10-2021
 """
 
 import pyautogui, logging, os, sys, cv2
 from PIL import Image
+pyautogui.PAUSE = 0
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s.%(msecs)03d: %(message)s', datefmt='%H:%M:%S')
+# logging.basicConfig(
+#     format='%(asctime)s,%(msecs)d %(levelname)-8s [%(name)s:%(filename)s:%(lineno)d] %(message)s',
+#     datefmt='%Y-%m-%d:%H:%M:%S',
+#     level=logging.DEBUG)
+
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(asctime)s.%(msecs)03d: [%(lineno)d] %(message)s', datefmt='%H:%M:%S')
 # logging.disable(logging.info) # uncomment to block debug log messages
 
 # TODO: Find algorithm for probability
@@ -21,6 +27,102 @@ def im_path(filename):
     """Returns the relative path for the image."""
     return os.path.join('images', filename)
 
+
+class MinesweeperCell():
+    """MinesweeperCell represents an individual cell in the MinesweeperSolver class."""
+
+    HIDDEN_COLOR_1 = (170, 215, 81)  # Alternatively: (162, 209, 73) although the tolerances should recognize this anyways
+    HIDDEN_COLOR_2 = (162, 209, 73)
+    BLANK_COLOR_1 = (215, 184, 153)  
+    BLANK_COLOR_2 = (229, 194, 159)
+    ONE_COLOR = (25, 118, 210)
+    TWO_COLOR = (69, 146, 67)
+    THREE_COLOR = (211, 47, 47)
+    FOUR_COLOR = (155, 85, 159)
+    FIVE_COLOR = (254, 144, 3)
+
+    COLOR_TOLERANCE = 20  # Distance in RGB value
+    POSITION_TOLERANCE = 5  # how many pixels around center will the color be searched for
+
+    HIDDEN_IMAGE = im_path('hidden_light.png')
+    BLANK_IMAGE = im_path('blank_light.png')
+    ONE_IMAGE = im_path('cell_1.png')
+    TWO_IMAGE = im_path('cell_2.png')
+    THREE_IMAGE = im_path('cell_3.png')
+    FOUR_IMAGE = im_path('cell_4.png')
+    FIVE_IMAGE = im_path('cell_5.png')
+
+    def __init__(self, ms_solver, coordinate):
+        logging.debug(f'Creating cell at ({coordinate[0], coordinate[1]})')
+        self.parent = ms_solver
+        self.cell_region = (None, None, None, None)
+        self.state = 'U'
+        self.color_position = None
+        self.coordinate = coordinate  # (x, y) tuple
+        self.neighbors = []
+
+    def __repr__(self):
+        return self.state
+
+    def identify_neighbors(self):
+        for x in range(self.coordinate[0] - 1, self.coordinate[0] + 2):
+            for y in range(self.coordinate[1] - 1, self.coordinate[1] + 2):
+                if (x, y) != self.coordinate:
+                    try:
+                        neighbor = self.parent.board[y][x]
+                        self.neighbors.append(neighbor)
+                        logging.debug(f'Neighbor for cell at ({self.coordinate[0], self.coordinate[1]}): ({x}, {y})')
+                    except IndexError:
+                        logging.debug('Neighbor does not exist. Likely edge/corner cell.')
+
+    def color_match(self, color):
+        logging.info(f'Checking color at {self.cell_region}')
+        img = pyautogui.screenshot(region=self.cell_region)
+        img_rgb = img.convert('RGB')
+
+        def within_range(test, check, tolerance):
+            if check - tolerance <= test <= check + tolerance:
+                return True
+            return False
+
+        search_all = False
+        if color in (self.BLANK_COLOR_1, self.BLANK_COLOR_2, self.HIDDEN_COLOR_1, self.HIDDEN_COLOR_2):
+            search_all = True
+
+        identified_color = []
+
+        for x in range(self.parent.cell_size // 2 - self.POSITION_TOLERANCE, self.parent.cell_size // 2 + self.POSITION_TOLERANCE):  # Checks color within certain x range
+            logging.debug(f'X: {x}, Y: {self.parent.cell_size // 2}')
+            rgb_pixel = img_rgb.getpixel((x, self.parent.cell_size // 2))
+            pixel_color_rgb = zip(rgb_pixel, color)
+            color_in_pixel = all(map(lambda x: within_range(x[0], x[1], self.COLOR_TOLERANCE), pixel_color_rgb))
+            if color_in_pixel and not search_all:
+                return True
+            if color_in_pixel and search_all:
+                identified_color.append(True)
+            if not color_in_pixel and search_all:
+                identified_color.append(False)
+        return False if not search_all else all(identified_color)
+
+
+    def update(self):
+        if self.color_match(self.HIDDEN_COLOR_1) or self.color_match(self.HIDDEN_COLOR_2):
+            self.state = '-'
+        elif self.color_match(self.BLANK_COLOR_1) or self.color_match(self.BLANK_COLOR_2):
+            self.state = '0'
+        elif self.color_match(self.ONE_COLOR):
+            self.state = '1'
+        elif self.color_match(self.TWO_COLOR):
+            self.state = '2'
+        elif self.color_match(self.THREE_COLOR):
+            self.state = '3'
+        elif self.color_match(self.FOUR_COLOR):
+            self.state = '4'
+        elif self.color_match(self.FIVE_COLOR):
+            self.state = '5'
+        else:
+            logging.debug('Failed to identify color.')
+            self.state = 'E'
 
 class MinesweeperSolver():
     """Creates a class that automatically solves Google Minesweeper if it can be found on the primary screen at runtime."""
@@ -70,6 +172,7 @@ class MinesweeperSolver():
         self.first_click = True
         self.color_movement = (None, None)
 
+
     def click_play(self):
         """Prepares the solver by bringing up the Minesweeper game if not already present."""
         logging.debug('Clicking play button! If already clicked, nothing should happen.')
@@ -84,6 +187,7 @@ class MinesweeperSolver():
             assert play_button_pos is not None, 'Google Minesweeper does not seem to be present on screen'
             pyautogui.click(play_button_pos)
 
+
     def find_menu_bar_region(self):
         """Finds the location of the default menu region, regardless of which difficulty it is set to."""
         logging.debug('Searching for menu region')
@@ -97,6 +201,7 @@ class MinesweeperSolver():
 
         logging.debug(f'Menu region is {self.menu_region}')
         return self.menu_region
+
 
     def set_difficulty(self):
         """Sets the difficulty according to the call and sets various instance variables dependent on difficulty."""
@@ -123,13 +228,13 @@ class MinesweeperSolver():
             self.color_movement = self.HARD_COLOR_MOVEMENT
 
         else:  # difficulty = medium
-            assert self.difficulty == 'medium', 'Difficulty does not seem to be valid?'
             pyautogui.moveRel(0, 45)  # Appropriate amount of movement to select medium
             pyautogui.click()
             self.board_size = self.BOARD_SIZE_MEDIUM
             self.cell_size = self.CELL_SIZE_MEDIUM
             self.game_region_size = self.GAME_REGION_SIZE_MEDIUM
             self.color_movement = self.MEDIUM_COLOR_MOVEMENT
+
 
     def find_game_region(self):
         """Finds the game region based off the menu region."""
@@ -138,39 +243,46 @@ class MinesweeperSolver():
         self.game_region = (self.menu_region[0], self.menu_region[1], *(self.game_region_size))
         logging.debug(f'Game region is {self.game_region}')
 
+
     def print_board(self):
         presentable_board = ('\n'.join([''.join(['{:4}'.format(item.state) for item in row]) for row in self.board]))
-        logging.debug('This is the initial board generated:\n' + presentable_board)
+        logging.debug('Current board:\n' + presentable_board)
+
 
     def generate_board(self):
         """Generates a 2D array to represent the actual Minesweeper board."""
         logging.debug('Generating board...')
-        self.board = [[MinesweeperCell(self) for x in range(self.board_size[0])] for y in range(self.board_size[1])] # Makes 2D array of MinesweeperCells
+        self.board = [[MinesweeperCell(self, (x, y)) for x in range(self.board_size[0])] for y in range(self.board_size[1])] # Makes 2D array of MinesweeperCells
 
         # Assigns to each MinesweeperCell in the 2D array a region on the screen
-        for y in self.board:
-            cell_height = self.board.index(y)
-            for x in y:
-                cell_width = y.index(x)
+        for row in self.board:
+            cell_height = self.board.index(row)
+            for cell in row:
+                cell_width = row.index(cell)
 
-                x.cell_region = (
+                cell.cell_region = (
                     self.menu_region[0] + cell_width * self.cell_size,
                     self.menu_region[1] + self.MENU_BAR_HEIGHT + cell_height * self.cell_size,
                     self.cell_size,
                     self.cell_size
                 )
+
+                cell.identify_neighbors()
         self.print_board()
+
 
     def update_board(self):
         """Updates each cell in the board to its current state."""
         logging.debug('Updating board!')
         for y in self.board:
-            for x in y:
-                x.update()
+            for cell in y:
+                cell.update()
         self.print_board()
+
 
     def evaluate_board(self):
         pass
+
 
     def click_and_flag(self):
         """Flags the 100% cells as determined in evaluate_board() and clicks the 0% cells."""
@@ -183,72 +295,17 @@ class MinesweeperSolver():
             logging.debug('Entering normal phase of clicking and flagging.')
 
 
-class MinesweeperCell():
-    """MinesweeperCell represents an individual cell in the MinesweeperSolver class."""
-
-    HIDDEN_COLOR = (170, 215, 81)  # Alternatively: (162, 209, 73) although the tolerances should recognize this anyways
-    BlANK_COLOR = (215, 184, 153)  # Alternatively: (229, 194, 159) This one might be beyond the tolerances
-    ONE_COLOR = (25, 118, 210)
-    TWO_COLOR = (69, 146, 67)
-    THREE_COLOR = (211, 47, 47)
-    FOUR_COLOR = (155, 85, 159)
-    FIVE_COLOR = (254, 144, 3)
-
-    def __init__(self, ms_solver):
-        self.cell_region = (None, None, None, None)
-        self.state = '-'
-        self.color_position = None
-        self.color_movement = ms_solver.color_movement
-
-    def __repr__(self):
-        return self.state
-
-    def color_match(self, color):
-        """Returns a boolean value whether the given color argument is found at the color position."""
-        # logging.debug(f'The color movement in MinesweeperCell is {self.color_movement}')
-        self.color_position = (
-            self.cell_region[0] + self.color_movement[0],
-            self.cell_region[1] + self.color_movement[1]
-            )
-
-        logging.debug(f'The color position of the cell is {self.color_position}')
-
-        # The pixelMatchesColor function in the pyautogui module doesn't seem to work in Python 3.8
-        # return pyautogui.pixelMatchesColor(*self.color_position, color)
-
-        pix = pyautogui.screenshot(region = (self.color_position[0], self.color_position[1], 1, 1))
-        color_position_rgb = pix.getpixel((0, 0))
-        # logging.debug(f'The RGB value at the position is {color_position_rgb}')
-        return True if color_position_rgb == color else False
-
-    def update(self):
-        """Updates the cell's state."""
-        # There's probably a better way to do this
-        if self.color_match(self.HIDDEN_COLOR):
-            pass
-        elif self.color_match(self.BlANK_COLOR):
-            self.state = '0'
-        elif self.color_match(self.ONE_COLOR):
-            self.state = '1'
-        elif self.color_match(self.TWO_COLOR):
-            self.state = '2'
-        elif self.color_match(self.THREE_COLOR):
-            self.state = '3'
-        elif self.color_match(self.FOUR_COLOR):
-            self.state = '4'
-        else:
-            self.state = '5'
-
-
 def main():
     """The main control loop"""
-    m = MinesweeperSolver('medium')
-    m.click_play()
-    m.find_menu_bar_region()
-    m.set_difficulty()
-    m.find_game_region()
-    m.generate_board()
-    m.click_and_flag()
-    m.update_board()
+    pass
+m = MinesweeperSolver('medium')
+m.click_play()
+m.find_menu_bar_region()
+m.set_difficulty()
+m.find_game_region()
+m.generate_board()
+m.click_and_flag()
+# m.update_board()
 
-main()
+# if __name__ == '__main__':
+#     main()
